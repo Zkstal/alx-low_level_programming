c408ca23dae08c74603322e70f3f716a45b13587
Zkstal <arhiffar@gmail.com>
2023-07-14 06:19:53 -0700

    T

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,9 +7,9 @@ int main(void)
 {
 	int number;
 
-	for (number = 0; number <= 9; number++)
+	for (number = 48; number < 58; number++)
 	{
-	putchar (number);
+		putchar(number);
 	}
 	putchar ('\n');
 	return (0);
--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-/** main entry point
- * Print the alphabet in reverse
- * Return 0
+/**
+ *  main - entry point Print the alphabet in reverse.
+ * Return: Return 0 (Success)
 */
 int main(void)
 {
 	char letter;
+
 	for (letter='z' ; letter >= 'a' ; letter--)
 	{
 		putchar(letter);
 	}
 	putchar('\n');
-	return 0
+	return (0);
 }
