1b7ba98104e9117583c4585d63ecb623e8e36c9b
Zkstal <arhiffar@gmail.com>
2023-07-14 06:10:29 -0700

    T

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
-/** main - entry point Print all single digit numbers.
+/**
+ * main - entry point Print all single digit numbers.
  * Return: Always returns 0 (Success)
  */
 int main(void)
 {
-    int number;
 
-    for (number = 0; number <= 9; number++)
-    {
-        printf("%d", number);
-    }
-
-    putchar('\n');
-    return 0;
+	int number;
+	
+	for (number = 0; number <= 9; number++)
+	{
+	printf("%d", number);
+	}
+	putchar('\n');
+	return (0);
 }
