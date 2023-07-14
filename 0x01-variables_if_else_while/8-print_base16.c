c22d04aae141f5a13ef03c7e16e6f155b171b984
Zkstal <arhiffar@gmail.com>
2023-07-14 06:27:12 -0700

    T

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Print base 16 digits. 
+ * main - Print base 16 digits.
  * Return: Return 0 (Success)
  */
 int main(void)
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
-/** main entry point
- * Print combination of single digit
- * Returns 0
-*/
+/** 
+ * main - entry point Print combination of single digit.
+ * Return: Return 0 (Success)
+ */
 int main(void)
 {
 	int number;
-		for (number =0;number <=9; number++)
+
+	for (number = 0; number <= 9; number++)
 	{
-		putchar('0' +number);
-	if (number!= 9)
+		putchar('0' + number);
+	if (number != 9)
 	{
 		putchar(',');
 		putchar(' ');
 	}
 	}
 	putchar('\n');
-	return 0;
+	return (0);
 }
