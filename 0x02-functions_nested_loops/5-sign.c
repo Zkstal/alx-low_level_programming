0f300871ec9ccd59002243c4c8634e42dc2563d4
Zkstal <arhiffar@gmail.com>
2023-07-18 06:21:31 -0700

    d

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,19 +7,18 @@
  */
 int print_sign(int n)
 {
-	if (n == 0)
+	if (n > 0)
 	{
-		_putchar("0\n");
-		return (0);
-	}
-	else if (n > 0)
-	{
-		_putchar("+\n");
+		_putchar('+');
 		return (1);
-	}
-	else if (n < 0)
+	} else if (n == 0)
+}
+		_putchar(48);
+		return (0);
+	} else if (n < 0)
 	{
-		_putchar("-\n");
+		_putchar('-');
 	}
 		return (-1);
 }
+
