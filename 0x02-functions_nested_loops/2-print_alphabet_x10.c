24d364a5eab94561973255b957a62a37bbb51cb3
Zkstal <arhiffar@gmail.com>
2023-07-18 05:16:39 -0700

    d

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,9 +10,9 @@ void print_alphabet_x10(void)
 	int l;
 	int t;
 
-	for (t = 1 ;t <= 10 ;t++)
+	for (t = 1 ; t <= 10 ; t++)
 	{
-		for (l = 97 ;l <= 122 ;l++)
+		for (l = 97 ; l <= 122 ; l++)
 		{
 			_putchar(j);
 		}
