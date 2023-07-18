cdd80b3046890a1111066994df31ced51428dc91
Zkstal <arhiffar@gmail.com>
2023-07-18 05:24:59 -0700

    d

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower - Check for lowcase character
+ *
+ * Return : 1 if lowercase else 0
+ */
+int _islower(int c);
+{
+	if (c = 65 && c =< 90)
+		return (1);
+	else
+		return (0);
+}
