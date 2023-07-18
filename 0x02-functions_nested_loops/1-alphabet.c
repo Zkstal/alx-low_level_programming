f55806df4f4407f1c824d6bf051d5840e1b06eb1
Zkstal <arhiffar@gmail.com>
2023-07-18 02:32:48 -0700

    d

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Print alphabet in lowercase
+ *
+ * Return : Return 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int i;
+	for ( i = 97 ; i <= 122 ; i++ )
+	{
+		_putchar(i);
+	}
+	_putchar("\n")
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - print alphabet x 10
+ *
+ * Return : Always return 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int l;
+	int t;
+
+	for ( t = 1 ; t <= 10; t++)
+	{
+		for ( l = 97 ; l <= 122 ; l++)
+		{
+			_putchar(j);
+		}
+		_putchar("\n");
+	}
+}
+
