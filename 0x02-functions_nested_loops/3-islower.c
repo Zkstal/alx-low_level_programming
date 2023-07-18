fb810b8da0ffa2a3f0708041bd50cc57dafe2664
Zkstal <arhiffar@gmail.com>
2023-07-18 05:30:16 -0700

    d

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * _islower - Check for lowcase character
- *
+ * c is the character to check
  * Return : 1 if lowercase else 0
  */
 int _islower(int c)
 {
-	if (c >= 65 && c <= 90)
+	if (c >= 97 && c <= 122)
 		return (1);
 	else
 		return (0);
