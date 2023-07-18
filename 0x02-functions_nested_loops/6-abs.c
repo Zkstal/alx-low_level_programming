62977db662049dd7a89f641448b10f17640ad242
Zkstal <arhiffar@gmail.com>
2023-07-18 07:02:32 -0700

    d

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_abs - Print absolute of a value
+ * @n: check the number
+ *
+ * Return: Absolute value
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n):
+	}
+	else
+		return (-n);
+}
