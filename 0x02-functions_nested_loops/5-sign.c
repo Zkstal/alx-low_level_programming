e2b14ecba30118468b0ffbc85ac7220ecffc9659
Zkstal <arhiffar@gmail.com>
2023-07-18 06:10:19 -0700

    d

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,25 @@
 #include "main.h"
 /**
+ * print_sign - Check number and add a sign corresponding to the number
+ * @n: The number to check to add sign
  *
- *
- *
+ *Return: 1 if positive 0 if it zero, -1 if negative
  */
+int print_sign(int n)
+{
+	if (n = "0")
+	{
+		return (0);
+		printf ("0\n");
+	}
+	else if (n > "1")
+	{
+		return (1);
+		printf ("+\n");
+	}
+	else
+	{
+		return (-1);
+		printf ("-\n");
+	}
+}
