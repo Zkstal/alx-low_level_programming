36493739175b86e25602aa6c421e461b905c65db
Zkstal <arhiffar@gmail.com>
2023-07-18 02:13:54 -0700

    d

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include "main.h"
 /** main - Print _putchar
  *
  * Return : Return 0
  */
 int main(void)
-{_putchar("_putchar\n");
+{
+	_putchar("_putchar\n");
 return (0);
 }
