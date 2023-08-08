a3dd7a1d50321eca0948db506ab07bbf11d27c36
Zkstal <arhiffar@gmail.com>
2023-08-08 03:31:38 -0700

    d

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,19 +9,14 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *cr;
+	char *str;
 	unsigned int i;
 
-	if (size == 0)
-		return (NULL);
-
-	cr = malloc(sizeof(c) * size);
-
-	if (cr == NULL)
+	str = malloc(sizeof(char) * size);
+	if (size == 0 || str == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
-		cr[i] = c;
-
-	return (cr);
+		str[i] = c;
+	return (str);
 }
