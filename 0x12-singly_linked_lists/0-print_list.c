3938a76c1b1b225ecdf6256757ea7b0262f2d628
Zkstal <arhiffar@gmail.com>
2023-08-24 13:48:21 -0700

    d

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,12 +9,12 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t count =0;
+	size_t s = 0;
 
 	while (h)
 	{
 		if (!h->str)
-			printf("[0] (nil)\n")
+			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
